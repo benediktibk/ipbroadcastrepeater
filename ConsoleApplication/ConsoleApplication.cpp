a786fdee25681a5439af93d5f54af426403f2f46
Benedikt Schmidt <benediktibk@gmail.com>
2020-11-08 20:26:35 +0100

    Try to implement Basic Example Usage of npcap SDK

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -1,21 +1,38 @@
-// ConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
 #include "pcap.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	pcap_if_t* alldevs;
+	pcap_if_t* d;
+	int i = 0;
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	/* Retrieve the device list from the local machine */
+	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL /* auth is not needed */, &alldevs, errbuf) == -1)
+	{
+		fprintf(stderr, "Error in pcap_findalldevs_ex: %s\n", errbuf);
+		exit(1);
+	}
+
+	/* Print the list */
+	for (d = alldevs; d != NULL; d = d->next)
+	{
+		printf("%d. %s", ++i, d->name);
+		if (d->description)
+			printf(" (%s)\n", d->description);
+		else
+			printf(" (No description available)\n");
+	}
+
+	if (i == 0)
+	{
+		printf("\nNo interfaces found! Make sure Npcap is installed.\n");
+		return 1;
+	}
+
+	/* We don't need any more the device list. Free it */
+	pcap_freealldevs(alldevs);
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
